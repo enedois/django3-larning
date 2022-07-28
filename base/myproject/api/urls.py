4d4d2dda70f782b86f73d4db60044ae2d0667963
Renan Zanesco Comoli <rzcrenan@gmail.com>
2022-07-28 20:01:47 +1000

    crud complete

--- base/myproject/api/urls.py
+++ base/myproject/api/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.getData)
+    path('',views.getData),
+    path('single/<int:pk>', views.item_detail),
+    path('add/', views.postData),
+    path('update/<int:pk>', views.updateData),
+    path('delete/<int:pk>', views.deleteData)
 ]
--- base/myproject/api/views.py
+++ base/myproject/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from projectBase.models import Item
 from .serializers import ItemSerializer
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET'])
@@ -12,3 +13,35 @@ def getData(request):
     #return Response(person)
 
     return Response(serializer.data)
+
+#gets the information
+@api_view(['GET'])
+def item_detail(request,pk):
+    items = Item.objects.filter(pk=pk)
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def postData(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+##updates the information
+@api_view(['PUT'])
+def updateData(request,pk):
+    items = Item.objects.filter(pk=pk).first()
+    serializer = ItemSerializer(items, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteData(request,pk):
+    items = Item.objects.get(pk=pk)
+    items.delete()
+    return Response()       
